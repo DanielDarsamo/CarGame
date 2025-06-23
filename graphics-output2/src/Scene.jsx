f63502a4352e0414288433870856771932769b53
Daniel <28514526+astrobeta@users.noreply.github.com>
2025-06-23 23:16:17 +0200

    set enviroment and textures

--- graphics-output2/src/Scene.jsx
+++ graphics-output2/src/Scene.jsx
@@ -7,14 +7,14 @@ import {Suspense} from "react";
 
 export function Scene(){
 return(
-< Suspense fallback={null}>
-    <Enviroment 
+    < Suspense fallback={null}>
+        <Enviroment 
             files={process.env.PUBLIC_URL+ "/textures/envmap.hdr"}
             background={"both"}
-            />
+        />
 
             <PerspectiveCamera makeDefault position={[-6, 3.9,6.21]} fov={40}/>
             <OrbitControls target={[-2.64, -0.71, 0.03]} />
-</Suspense>
-);
+    </Suspense>
+    );
 }
