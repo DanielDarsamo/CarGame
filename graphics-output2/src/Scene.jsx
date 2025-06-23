70b680ca6774a9581f5584970e71c24d0fa8bc58
Daniel <28514526+astrobeta@users.noreply.github.com>
2025-06-23 18:36:18 +0200

    added boilerplate of the scene and  the orbit camera controls

---
+++ graphics-output2/src/Scene.jsx
@@ -0,0 +1,20 @@
+import{
+    Enviroment, 
+    OrbitControls, 
+    PerspectiveCamera, 
+} from "@react-three/drei"; 
+import {Suspense} from "react"; 
+
+export function Scene(){
+return(
+< Suspense fallback={null}>
+    <Enviroment 
+            files={process.env.PUBLIC_URL+ "/textures/envmap.hdr"}
+            background={"both"}
+            />
+
+            <PerspectiveCamera makeDefault position={[-6, 3.9,6.21]} fov={40}/>
+            <OrbitControls target={[-2.64, -0.71, 0.03]} />
+</Suspense>
+);
+}
